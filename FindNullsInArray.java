db1809865ec30eb6d7a6882f971f0e38128d1f1e
Polina-Mikhaiovna <umrilovapolina@gmail.com>
2025-07-20 22:27:48 +0300

    Find nuls in array. Now with set ArrayList<Integer>

--- FindNullsInArray.java
+++ FindNullsInArray.java
@@ -1,17 +1,20 @@
+import java.util.ArrayList;
+
 public class FindNullsInArray {
     public static void main(String[] args) {
         Integer[] array = {1, 2, 3, 4, null, 6, null};
-        checkNullInArray(array);
+        ArrayList<Integer> elements = checkNullInArray(array);
+        System.out.println("Index of null element/s is/are: " + elements);
     }
-    
-    public static void checkNullInArray(Integer[] array){
-        StringBuilder indexes = new StringBuilder();
+
+    public static ArrayList<Integer> checkNullInArray(Integer[] array){
+        ArrayList<Integer> elements = new ArrayList<>();
         for (int i = 0; i < array.length; i++) {
             if (array[i] == null){
-                indexes.append(Integer.toString(i)).append(" ");
+                elements.add(i);
             }
         }
-        System.out.printf("Element with index %s is null\n", indexes);
+        return elements;
     }
     
 }
