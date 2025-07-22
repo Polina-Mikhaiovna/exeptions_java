dd25f7a8a89455b2e85a283ffd09e8216cf9e219
Polina-Mikhaiovna <umrilovapolina@gmail.com>
2025-07-22 20:17:52 +0300

    Some apdate in sumTwoArrays, add differece

--- SumTwoArrays.java
+++ SumTwoArrays.java
@@ -4,27 +4,43 @@ public class SumTwoArrays {
         int[] arr2 = { 5, 4, 3, 2, 1 };
 
         try {
-            int len = arr1.length;
-            int[] res = new int[len];
-            res = sumArrays(arr1, arr2);
-            for (int i = 0; i < res.length; i++) {
-                System.out.printf("%d ", res[i]);
-            }
-            System.out.println();
+            checkArraysLength(arr1, arr2);
+            print2DArray(sumArrays(arr1, arr2));
+            print2DArray(difArrays(arr1, arr2));
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
         }
+
     }
 
     public static int[] sumArrays(int[] arr1, int[] arr2) {
-        if (arr1.length != arr2.length) {
-            throw new IllegalArgumentException("Arrays are not equal length");
-        }
         int len = arr1.length;
         int[] res = new int[len];
         for (int i = 0; i < len; i++) {
             res[i] = arr1[i] + arr2[i];
         }
         return res;
+    }
+
+    public static int[] difArrays(int[] arr1, int[] arr2) {
+        int len = arr1.length;
+        int[] res = new int[len];
+        for (int i = 0; i < len; i++) {
+            res[i] = arr1[i] - arr2[i];
+        }
+        return res;
+    }
+
+    public static void checkArraysLength(int[] arr1, int[] arr2) {
+        if (arr1.length != arr2.length) {
+            throw new IllegalArgumentException("Arrays are not equal length");
+        }
+    }
+
+    public static void print2DArray(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.printf("%d ", array[i]);
+        }
+        System.out.println();
     }
 }
