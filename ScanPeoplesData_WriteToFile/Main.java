578a000864f250863b1be924f790b26ce21ca864
Polina-Mikhaiovna <umrilovapolina@gmail.com>
2025-07-30 16:13:42 +0300

    Scanning people's data and write it into files done, but not perfect

--- ScanPeoplesData_WriteToFile/Main.java
+++ ScanPeoplesData_WriteToFile/Main.java
@@ -11,44 +11,32 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        //Фамилия Имя Отчество(строки) датарождения(dd.mm.yyyy) номертелефона(целое беззнаковое число без форматирования) пол(символ латиницей f или m)
-        //String scanInputData(Scanner scanner)
-        //int checkData(String data) вернуть код ошибки - меньше данных больше или ок
-        //String parsString(String data)
-        //          Если форматы данных не совпадают, нужно бросить исключение, соответствующее типу проблемы
-        //          пользователю выведено сообщение с информацией, что именно неверно
-        //          Если всё введено и обработано верно, должен создаться файл с названием, равным фамилии
-        // Path createFileWithName(String name)
-        //void writeDataToFile(Path path, String[][] data) проблемы с чтением-записью в файл, исключение должно
-        //                              быть корректно обработано, пользователь должен увидеть стектрейс ошибки
 
         System.out.println("\n\n************************************************************************");
         System.out.println("********Введите ваши данные через пробел в произвольном порядке********");
         System.out.println("************************************************************************\n");
-        System.out.println("Фамилия Имя Отчество дата рождения(в формате дд.мм.гггг) номер телефона(только цифры без форматирования)"
-                + " пол(символ латиницей f или m):\n\n");
-                
+        System.out.println(
+                "Фамилия Имя Отчество дата рождения(в формате дд.мм.гггг) номер телефона(только цифры без форматирования)"
+                        + " пол(символ латиницей f или m):\n\n");
         String data;
         String[] dataArray;
 
         try (Scanner scanner = new Scanner(System.in);) {
             data = scanInputString(scanner);
             dataArray = parsString(data);
-            try{
+            try {
                 checkDataFormat(dataArray);
+                Path path = createFileWithName(dataArray[0] + ".txt");
+                writeDataToFile(path, dataArray);
                 System.out.println("Даные корректны. Записываем их в файл \"Ваша_фамилия.txt\"");
             } catch (RuntimeException e) {
                 System.out.println(e.getMessage());
             }
-            Path path = createFileWithName(dataArray[0] + ".txt");
-            writeDataToFile(path, dataArray);
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
-        
-
     }
-    
+
     public static String scanInputString(Scanner scanner) {
         String data = new String();
         boolean isData = false;
@@ -93,7 +81,7 @@ public class Main {
 
             if (isValidDate(current)) {
                 tempArr[3] = current;
-            } else if (current.equals("f") || current.equals("m")) {
+            } else if (current.equalsIgnoreCase("f") || current.equalsIgnoreCase("m")) {
                 tempArr[5] = current;
             } else if (current.matches("\\d+")) {
                 tempArr[4] = current;
@@ -104,7 +92,8 @@ public class Main {
                     tempArr[1] = current;
                 else if (tempArr[2] == null)
                     tempArr[2] = current;
-        }}
+            }
+        }
         System.out.println(Arrays.toString(tempArr));
         return tempArr;
     }
@@ -155,7 +144,8 @@ public class Main {
             fw.write("\n");
             fw.flush();
         } catch (IOException ex) {
-            System.out.println(ex.getMessage());
+            System.out.println(ex.getStackTrace());
         }
+
     }
 }
